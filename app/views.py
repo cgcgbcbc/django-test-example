04bc355f5b55155fad9b490d9952fa94159f755d
cgcgbcbc <cgcgbcbc@163.com>
2015-12-01 20:39:58 +0800

    add url for wechat/

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
 from app.models import Activity
+import xml.etree.cElementTree as ET
 
 
 def fetch_ticket(request: HttpRequest):
@@ -14,3 +14,7 @@ def fetch_ticket(request: HttpRequest):
         return HttpResponse('成功', content_type='text/plain')
     else:
         return HttpResponse('失败', content_type='text/plain')
+
+
+def wechat_handler(request):
+    return HttpResponse('<xml></xml>', content_type='text/xml')
--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,9 +9,10 @@ import xml.etree.cElementTree as ET
 
 class TestExample(TestCase):
     def setUp(self):
-        self.client = WechatClient()
+        self.client = WechatClient(wechat_url='/wechat/')
 
     def test_fetch_ticket(self):
         response = self.client.send_wechat_text('抢票')
-        msg = ET.fromstring(str(response.content))
+        self.assertEqual(response.status_code, 200)
+        msg = ET.fromstring(response.content.decode())
         # TODO add assert.
